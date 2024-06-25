f6aaee66e8a15ee0f923ae6120090f432d463b04
Ron <ronzano123@gmail.com>
2024-06-26 01:45:52 +0300

    is the prettier working?

--- apps/client/.eslintrc.cjs
+++ apps/client/.eslintrc.cjs
@@ -1,18 +1,22 @@
 module.exports = {
   env: { browser: true, es2020: true },
-  extends: ["plugin:react-hooks/recommended", "@repo/eslint-config/index.js", "airbnb-typescript"],
-  ignorePatterns: ["dist", ".eslintrc.cjs"],
-  parser: "@typescript-eslint/parser",
-  plugins: ["react-refresh"],
+  extends: ['plugin:react-hooks/recommended', 'plugin:react/recommended', '@repo/eslint-config/index.js', 'airbnb', 'airbnb-typescript', 'prettier'],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react-refresh', 'prettier'
+  ],
   parserOptions: {
-    sourceType: "module",
+    sourceType: 'module',
     ecmaVersion: 2020,
-    project: ["./tsconfig.app.json"],
+    project: ['./tsconfig.app.json'],
   },
   rules: {
-    "react-refresh/only-export-components": [
-      "warn",
-      { allowConstantExport: true },
-    ],
-  },
+    'prettier/prettier': 'error',
+    "react/jsx-uses-react": "off",
+    "react/react-in-jsx-scope": "off",
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true }
+    ]
+  }
 };
