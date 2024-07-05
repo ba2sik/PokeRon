f0d257303adf608e9340a7424077f988acd6e57e
Ron <ronzano123@gmail.com>
2024-07-06 01:22:00 +0300

    adding tailwind

--- apps/client/src/pages/home/Home.tsx
+++ apps/client/src/pages/home/Home.tsx
@@ -5,6 +5,10 @@ import PokeCard from "../../components/PokeCard/PokeCard";
 function Home() {
   return (
     <>
+      <h1 className="text-3xl font-bold text-red-700 underline text-center">
+        Hello world!
+      </h1>
+
       <h1> PokéRon</h1>
       <GoButton type="button">GO</GoButton>
       <div className="cards">
