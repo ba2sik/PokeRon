e269debd106a3f552897f1c6882012dd5831bfd9
Ron <ronzano123@gmail.com>
2024-07-08 17:27:35 +0300

    exporting icon to component

--- apps/client/src/components/Search/Search.tsx
+++ apps/client/src/components/Search/Search.tsx
@@ -1,3 +1,5 @@
+import SearchIcon from './SearchIcon';
+
 const Search = () => {
   return (
     <label className="input input-bordered m-4 flex items-center gap-2">
@@ -6,19 +8,7 @@ const Search = () => {
         className="grow"
         placeholder="Search"
       />
-      {/* CR: I would put svg in .svg file and import it and use it like a component */}
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 16 16"
-        fill="currentColor"
-        className="h-4 w-4 opacity-70"
-      >
-        <path
-          fillRule="evenodd"
-          d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
-          clipRule="evenodd"
-        />
-      </svg>
+      <SearchIcon />
     </label>
   );
 };
