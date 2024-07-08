adc3d84173dbd0b5e45582bd8e1c07b017218c2d
Ron <ronzano123@gmail.com>
2024-07-09 00:35:51 +0300

    removing cr comment

--- apps/client/src/requests/getPokemons.ts
+++ apps/client/src/requests/getPokemons.ts
@@ -1,8 +1,6 @@
 import { PokemonApi } from '@repo/poke-client';
 
 export const getPokemonsSummaries = async () => {
-  // CR: create pokemonClient so that it would look like:
-  // const { data } = await pokemonClient.get('/pokemon');
   const api = new PokemonApi();
   const {
     data: { results: pokemonsSummaries = [] },
