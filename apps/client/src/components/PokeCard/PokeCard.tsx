3a3254478e03c09033167bfe5e0ff56a55c6e587
Ron <ronzano123@gmail.com>
2024-07-08 14:13:42 +0300

    a little ux changes

--- apps/client/src/components/PokeCard/PokeCard.tsx
+++ apps/client/src/components/PokeCard/PokeCard.tsx
@@ -1,19 +1,17 @@
-import React from "react";
+import React from 'react';
 
 export type PokeCardProps = {
   name: string;
 };
 
+const a = [1, 2, 3];
+
 const PokeCard: React.FC<PokeCardProps> = ({ name }) => {
   return (
-    <div className="card bg-base-100 m-4 w-60 items-center shadow-xl">
-      <img
-        src={`https://img.pokemondb.net/artwork/${name}.jpg`}
-        alt="shoes"
-        className="h-40"
-      />
+    <div className="card bg-base-100 m-4 w-60 items-center shadow-xl hover:scale-105 hover:cursor-pointer transition duration-300 ease-in-out">
+      <img src={`https://img.pokemondb.net/artwork/${name}.jpg`} alt="shoes" className="h-40" />
       <div className="card-body">
-        <h2 className="card-title">{name}</h2>
+        <h2 className="card-title capitalize">{name}</h2>
         <p>{`This is ${name}`}</p>
         <div className="card-actions justify-end">
           <div className="badge badge-outline">Fashion</div>
