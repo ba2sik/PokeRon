46457757acec1b4aa023461f046a60579279c14e
Ron <ronzano123@gmail.com>
2024-07-08 19:19:18 +0300

    adding another type for api request

--- apps/client/src/pages/home/Home.tsx
+++ apps/client/src/pages/home/Home.tsx
@@ -2,38 +2,36 @@ import Pokedex from '../../components/Pokedex/Pokedex';
 import Search from '../../components/Search/Search';
 import { useQuery } from 'react-query';
 import { POKE_API_URL } from '../../constants/api';
-import { BasicPokemon } from '../../types/api/apiTypes';
+import { BasicPokemon, BasicPokemonList } from '../../types/api/apiTypes';
 import axios from 'axios';
 
 // CR: should be in requests folder and not here
-const getPokemons = async () => {
+const getPokemons = async (): Promise<BasicPokemon[]> => {
   // CR: create pokemonClient so that it would look like:
   // const { data } = await pokemonClient.get('/pokemon');
-  const response = await axios.get(`${POKE_API_URL}/pokemon`);
+  const response = await axios.get<BasicPokemonList>(`${POKE_API_URL}/pokemon`);
   return response.data.results;
 };
 
 function Home() {
   // CR: should be in a hook in a separated file. an example is down below
   const {
-    data: pokemons,
+    data: pokemons, // Ron: how do i add type here?
     isLoading,
     isError,
   } = useQuery('pokemons', getPokemons);
   // CR: and it would look like this here:
   // const { data, isLoading, isError } = usePokemons();
   if (isLoading) return <h1>Loading...</h1>;
-  if (isError) return <h1>Error...</h1>;
+  if (isError || !pokemons) return <h1>Error...</h1>;
 
-  // CR: I would infer the type like this:
-  // const pokemonNames = data.results.map<string>(
   // Also I would create a generic extract function so that it would look like this:
   // const pokemonNames = extract(data.results, 'name');
   // OR:
   // const pokemonNames = extract(data.results, ({ name }) => name);
   // OR:
   // const pokemonNames = extract(data.results, pokemon => pokemon.name);
-  const pokemonNames: string[] = pokemons.map(
+  const pokemonNames = pokemons.map<string>(
     (pokemon: BasicPokemon) => pokemon.name,
   );
 
