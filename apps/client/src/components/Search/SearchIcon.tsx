5d40edaaf30b54118df3c3eb2372f97109b9d45b
Ron <ronzano123@gmail.com>
2024-07-09 14:35:44 +0300

    enabling props injection from outside

--- apps/client/src/components/Search/SearchIcon.tsx
+++ apps/client/src/components/Search/SearchIcon.tsx
@@ -1,10 +1,13 @@
-const SearchIcon = () => {
+import React, { SVGProps } from 'react';
+
+const SearchIcon: React.FC<SVGProps<SVGSVGElement>> = (props) => {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 16 16"
       fill="currentColor"
       className="h-4 w-4 opacity-70"
+      {...props}
     >
       <path
         fillRule="evenodd"
