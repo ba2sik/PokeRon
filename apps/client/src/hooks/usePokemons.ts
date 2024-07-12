24dcff53e90fd5a07ddb9b5881c45d322a79fdde
Ron <ronzano123@gmail.com>
2024-07-12 16:28:15 +0300

    migrating to tanstack/react-query 😆

--- apps/client/src/hooks/usePokemons.ts
+++ apps/client/src/hooks/usePokemons.ts
@@ -1,10 +1,10 @@
-import { QueryOptions, useQuery, UseQueryResult } from 'react-query';
+import { QueryOptions, useQuery, UseQueryResult } from '@tanstack/react-query';
 import { getBasicPokemons } from '../requests/getPokemons';
 import { BasicPokemon } from '../types/pokemons';
 
 export const usePokemons = (options?: QueryOptions): UseQueryResult<BasicPokemon[]> => {
   return useQuery({
-    queryKey: 'pokemons',
+    queryKey: ['pokemons'],
     queryFn: getBasicPokemons,
     ...options,
   });
