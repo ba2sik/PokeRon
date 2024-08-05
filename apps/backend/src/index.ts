61b3e7511c9a80b24a217ca3cd1af4dbe8617c3c
Ron <ronzano123@gmail.com>
2024-08-05 12:15:09 +0300

    some basic express middlewares

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,10 +1,17 @@
 import express, { Express, Request, Response } from 'express';
 import { PrismaClient } from '@prisma/client';
+import cors from 'cors';
+import helmet from 'helmet';
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
 
 export const prisma = new PrismaClient();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+app.use(helmet());
 
 app.get('/', async (req: Request, res: Response) => {
   const favoriteCards = await prisma.favoriteCard.findMany();
