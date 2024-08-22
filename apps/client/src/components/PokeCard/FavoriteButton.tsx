0f9e22c5b8b69ebace97f2d4b5d9f3759bbbf19a
Ron <ronzano123@gmail.com>
2024-08-22 22:33:11 +0300

    styling favorite buttons colors

--- apps/client/src/components/PokeCard/FavoriteButton.tsx
+++ apps/client/src/components/PokeCard/FavoriteButton.tsx
@@ -1,7 +1,6 @@
 import React, { FC } from 'react';
 import HeartFilled from '/heart-filled.svg';
 import HeartOutline from '/heart-outline.svg';
-import { twMerge } from 'tailwind-merge';
 
 type FavoriteButtonProps = {
   isFavorite: boolean;
@@ -13,10 +12,7 @@ export const FavoriteButton: FC<FavoriteButtonProps> = ({ isFavorite, onClick, .
     <div {...props}>
       <button
         onClick={onClick}
-        className={twMerge(
-          'w-9 h-9 flex items-center justify-center rounded-full shadow-lg bg-white border border-gray-300 transition-colors duration-200',
-          isFavorite ? 'bg-red-500 border-transparent' : '',
-        )}
+        className="w-9 h-9 flex items-center justify-center rounded-full shadow-all-sides bg-white transition-colors duration-200"
       >
         {isFavorite ? (
           <img
@@ -28,7 +24,7 @@ export const FavoriteButton: FC<FavoriteButtonProps> = ({ isFavorite, onClick, .
           <img
             src={HeartOutline}
             alt="Outline Heart"
-            className="w-5 h-5"
+            className="w-5 h-5 fill-red-600"
           />
         )}
       </button>
--- apps/client/tailwind.config.js
+++ apps/client/tailwind.config.js
@@ -3,7 +3,11 @@ module.exports = {
   mode: 'jit',
   content: ['index.html', './src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        'all-sides': '0 0 20px rgba(0, 0, 0, 0.1)',
+      },
+    },
   },
   plugins: [require('@tailwindcss/typography'), require('daisyui')],
 };
